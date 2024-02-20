3b8c5b8602a7619e1254e74cc1852da1e70c9098
Dmytro Hapchenko <dmytrohapchenko@gmail.com>
2024-02-20 18:38:34 +0200

    Updating main_page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request) -> HttpResponse:
+    context: dict = {
+        'title': 'Home',
+        'content': 'Main Page',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': True,
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request) -> HttpResponse:
+    return HttpResponse("About Page")
